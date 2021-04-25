8459280d93c343a2d283393d47f569cea45e4b52
kahmnkk <kyuhh1214@naver.com>
2021-04-25 14:20:33 +0900

    add. SplashActivity

--- client/app/src/main/java/com/indilist/photoreceipt/SplashActivity.java
+++ client/app/src/main/java/com/indilist/photoreceipt/SplashActivity.java
@@ -1,23 +1,30 @@
-package com.indilist.photoreceipt.ui;
+package com.indilist.photoreceipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.indilist.photoreceipt.MainActivity;
-import com.indilist.photoreceipt.R;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(runnable, 1000);
     }
 }
