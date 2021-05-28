ecc838afbc584c342fbbb5b11e7492ef6d59d73a
kowar72 <moonsh72@ajou.ac.kr>
2021-05-29 03:14:30 +0900

    R,G,B Boost updated Touch autofocus updated hdr on/off updated now we can choose back/front camera

---
+++ client/app/src/main/java/com/indilist/photoreceipt/DBHelper.java
@@ -0,0 +1,28 @@
+package com.indilist.photoreceipt;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "photorecipe.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table photo (_idx integer primary key autoincrement, fname text, filter text);");
+        db.execSQL("create table filters (_idx integer primary key autoincrement, filter text);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists photo");
+        db.execSQL("drop table if exists filters");
+    }
+}
