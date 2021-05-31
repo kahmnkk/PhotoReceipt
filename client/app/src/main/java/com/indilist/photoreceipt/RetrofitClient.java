3670547cc3eaacba6f2d40bbe6ac0295b9ce4c01
kahmnkk <kyuhh1214@naver.com>
2021-06-01 04:18:28 +0900

    fix. rollback Retrofit BASE_URL

--- client/app/src/main/java/com/indilist/photoreceipt/RetrofitClient.java
+++ client/app/src/main/java/com/indilist/photoreceipt/RetrofitClient.java
@@ -4,8 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-//    private static final String BASE_URL = "http://3.21.35.164:8080/";
-    private static final String BASE_URL = "http://192.168.1.2:8080/";
+    private static final String BASE_URL = "http://3.21.35.164:8080/";
+//    private static final String BASE_URL = "http://192.168.1.2:8080/";
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
